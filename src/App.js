ee1452c2451f660b020d49f3348017b98a00fd34
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-03 11:52:57 +0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import RandomNameGenerator from "./components/RandomNameGenerator";
+
+const App = () => {
+  return (
+    <div className="App">
+      <RandomNameGenerator />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/RandomNameGenerator.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+const btnStyle = {
+  padding: "10px 60px",
+  backgroundColor: "green",
+  color: "white",
+  margin: "10px 0",
+  borderRadius: "10px",
+  border: "none",
+  cursor: "pointer",
+  fontSize: "30px",
+  boxShadow: "0 0 5px 5px gray"
+};
+
+const RandomNameGenerator = () => {
+  const [name, setName] = useState("______");
+
+  const handleNameChange = () => {
+    const names = ["Dehan", "Musfiq", "Rahim"];
+    const randInt = Math.floor(Math.random() * names.length);
+    setName(names[randInt]);
+  };
+
+  return (
+    <div>
+      <h1>{name}</h1>
+
+      <button style={btnStyle} onClick={handleNameChange}>
+        Generate Name
+      </button>
+    </div>
+  );
+};
+
+export default RandomNameGenerator;
